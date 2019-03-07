ba6a754b1a9902de4854220e39fe12a44a689891
Justice <justice.faustin@gmail.com>
2019-03-07 18:28:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React by Jufa!</h1>
+          <h1 className="App-title">Welcome to React by Jufa!- This is True Man!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
